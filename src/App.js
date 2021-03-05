bee17a3567bd234384941cdd727cbae9076c1aea
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-04 23:37:38 -0800

    Added pages and components files and folders.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import Footer from "./components/Footer"
+import Nav from "./components/Nav";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Main from "./pages/Main";
+import Projects from "./pages/Projects"
 
 function App() {
+  const display = () => {
+    const route = window.location.pathname
+      if(route.includes("/")){
+        return <Main />
+      } else if(route.includes("/about")){
+        return <About />
+      } else if(route.includes("/projects")){
+        return <Projects />
+      } else if(route.includes("/contact")){
+        return <Contact />
+      } else {
+        return <Main />
+      }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      {display()}
+      <Footer />
+    </>
   );
 }
 
