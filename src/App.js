1c5f5b876b419376297aa3f18125d1b5b3fc5fc9
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-05 22:03:22 -0800

    Fixed routes and added a Resume page.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import Nav from "./components/Nav";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Main from "./pages/Main";
-import Projects from "./pages/Projects"
+import Projects from "./pages/Projects";
+import Resume from "./pages/Resume";
 
 function App() {
   const display = () => {
     const route = window.location.pathname
-      if(route.includes("/")){
-        return <Main />
-      } else if(route.includes("/about")){
+      if(route.includes("/about")){
         return <About />
       } else if(route.includes("/projects")){
         return <Projects />
       } else if(route.includes("/contact")){
         return <Contact />
+      } else if(route.includes("/resume")){
+        return <Resume />
       } else {
         return <Main />
       }
