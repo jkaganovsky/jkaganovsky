a817d048c293205f3f474e9673ca1aadef4e2eed
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-05 22:09:03 -0800

    Added Main landing page function and html tags.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,7 +1,21 @@
 function Main() {
   return(
     <>
+      <div className="site-wrapper">
 
+        <div className="inner cover bg-white">
+          <h1 className="name display-1 cover-heading text-center"> JAILANIE KAGANOVSKY </h1>
+          <h2 className="index-title text-center">Research Coordinator & Full-Stack Web Developer</h2>
+        </div>
+
+        <div className="d-flex flex-wrap justify-content-evenly">
+          <a className="text-white p-4" href="about">ABOUT ME</a>
+          <a className="text-white p-4" href="projects">PROJECTS</a>
+          <a className="text-white p-4" href="contact">CONTACT ME</a>
+          <a className="text-white p-4" href="/public/assets/Resume.pdf" target="_blank">RESUME</a>
+        </div>
+
+      </div>
     </>
   )
 }
