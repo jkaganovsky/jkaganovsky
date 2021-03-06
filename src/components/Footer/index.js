f73f4dda8d17609cab94d67fc52253f5ce00a530
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-05 23:57:26 -0800

    Added html tag content.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,10 @@
 function Footer() {
   return(
     <>
-
+      <div className="card-footer bg-white text-center">
+        <span className="iconify" data-icon="noto-v1:copyright" data-inline="false"></span>
+        2021 by Jailanie Kaganovsky
+      </div>
     </>
   )
 }
