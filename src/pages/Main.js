a3190e7eca78bd16ea66ddedd2e81ae2cd59f38c
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-06 17:49:30 -0800

    Edited bootstrap syntax for mobile accessibility.

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,20 +1,28 @@
+/* eslint-disable react/jsx-no-target-blank */
 function Main() {
   return(
     <>
-      <div className="site-wrapper">
-
-        <div className="inner cover bg-white">
-          <h1 className="name display-1 cover-heading text-center"> JAILANIE KAGANOVSKY </h1>
-          <h2 className="index-title text-center">Research Coordinator & Full-Stack Web Developer</h2>
+      <div className="card my-5 py-5 bg-white">
+        <div className="row">
+          <div className="col-lg">
+            <h1 className="name text-center"> JAILANIE KAGANOVSKY </h1>
+          </div>
         </div>
 
+        <div className="row">
+          <div className="col-lg">
+            <h2 className="title text-center">Research Coordinator & Full-Stack Web Developer</h2>
+          </div>
+        </div>
+      </div>
+
+      <div className="container-fluid pb-5">
         <div className="d-flex flex-wrap justify-content-evenly">
-          <a className="text-white p-4" href="about">ABOUT ME</a>
-          <a className="text-white p-4" href="projects">PROJECTS</a>
-          <a className="text-white p-4" href="contact">CONTACT ME</a>
-          <a className="text-white p-4" href="/public/assets/Resume.pdf" target="_blank">RESUME</a>
+          <a className="text-white fw-bolder fs-3 p-4" href="about">ABOUT ME</a>
+          <a className="text-white fw-bolder fs-3 p-4" href="projects">PROJECTS</a>
+          <a className="text-white fw-bolder fs-3 p-4" href="contact">CONTACT ME</a>
+          <a className="text-white fw-bolder fs-3 p-4" href="resume">RESUME</a>
         </div>
-
       </div>
     </>
   )
