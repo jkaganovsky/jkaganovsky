22fd29c0ef1672182a1c0359feba84f59978f2db
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-06 19:39:13 -0800

    Removed unused public files.

--- public/assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(() => {
-
-    $('.navbar-toggler').on('click', function(event) {
-		event.preventDefault();
-		$(this).closest('.navbar-minimal').toggleClass('open');
-    })
-    
-});
