4393f33db5397b6709300d5988b5c755efa94e6b
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-06 18:50:26 -0800

    Changed bootstrap view to center-aligned.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,27 +4,27 @@ function Contact() {
   return(
     <>
     <Nav />
-      <div className="row justify-content-end p-5 m-5">
-        <div className="col-5 my-5 bg-white">
+      <div className="container-fluid d-flex flex-wrap justify-content-center p-5 m-5">
+        <div className="col-8 my-5 bg-white">
 
-            <p className="fs-1 text-right pt-2">CONTACT ME</p>
+            <p className="fs-1 text-center pt-2">CONTACT ME</p>
 
-            <a className="nav-link h5 text-right" href="mailto:jnctw33ty@gmail.com">
+            <a className="nav-link h5 text-center" href="mailto:jnctw33ty@gmail.com">
               <span className="iconify" data-icon="entypo:email" data-inline="false"></span>
               E-mail
             </a>
 
-            <a className="nav-link h5 text-right" href="https://www.linkedin.com/in/jailanie-kaganovsky-573a00116/">
+            <a className="nav-link h5 text-center" href="https://www.linkedin.com/in/jailanie-kaganovsky-573a00116/">
               <span className="iconify" data-icon="mdi-linkedin" data-inline="false"></span>
               LinkedIn
             </a>
 
-            <a className="nav-link h5 text-right" href="https://github.com/jkaganovsky">
+            <a className="nav-link h5 text-center" href="https://github.com/jkaganovsky">
               <span className="iconify" data-icon="entypo-social:github" data-inline="false"></span>
               Github
             </a>
 
-            <a className="nav-link h5 text-right" href="resume">
+            <a className="nav-link h5 text-center" href="resume">
               <span className="iconify" data-icon="zmdi:collection-pdf" data-inline="false"></span>
               Resume
             </a>
