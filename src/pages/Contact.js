b78c8463dfbe3f6030207530f5d1a6d437aec07d
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-03-06 18:49:02 -0800

    Fixed bootstrap css styling.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,31 +1,33 @@
+import Nav from "../components/Nav";
+
 function Contact() {
   return(
     <>
-      <div className="container">
-        <div className="d-flex flex justify-content-between m-5">
-          <div className="contact col-4 bg-white">
-            <p className="fs-1 text-right">CONTACT ME</p>
+    <Nav />
+      <div className="row justify-content-end p-5 m-5">
+        <div className="col-5 my-5 bg-white">
+
+            <p className="fs-1 text-right pt-2">CONTACT ME</p>
 
             <a className="nav-link h5 text-right" href="mailto:jnctw33ty@gmail.com">
               <span className="iconify" data-icon="entypo:email" data-inline="false"></span>
-                      E-mail
+              E-mail
             </a>
 
             <a className="nav-link h5 text-right" href="https://www.linkedin.com/in/jailanie-kaganovsky-573a00116/">
               <span className="iconify" data-icon="mdi-linkedin" data-inline="false"></span>
-                      LinkedIn
+              LinkedIn
             </a>
 
             <a className="nav-link h5 text-right" href="https://github.com/jkaganovsky">
               <span className="iconify" data-icon="entypo-social:github" data-inline="false"></span>
-                      Github
+              Github
             </a>
 
-            <a className="nav-link h5 text-right" href="/assets/Resume.pdf">
+            <a className="nav-link h5 text-right" href="resume">
               <span className="iconify" data-icon="zmdi:collection-pdf" data-inline="false"></span>
-                      Resume
+              Resume
             </a>
-          </div>
         </div>
       </div>
     </>
