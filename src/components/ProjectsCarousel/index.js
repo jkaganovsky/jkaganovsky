d31815f2866962c8045aa608b26525fc640c623d
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-05-05 19:40:30 -0700

    Removed Table and added coin creatif and google book search projects.

--- src/components/ProjectsCarousel/index.js
+++ src/components/ProjectsCarousel/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable jsx-a11y/alt-text */
-import { Carousel, Table }from "react-bootstrap";
+import { Carousel }from "react-bootstrap";
 
 function bootstrapCarousel() {
   return (
@@ -14,6 +14,18 @@ function bootstrapCarousel() {
 
       <div className="container-fluid">
         <Carousel interval={5000} keyboard={false} pauseOnHover={true}>
+          <Carousel.Item style={{'height': "300px"}}>
+            <img style={{'height': "300px"}} className="d-block w-100" src="/assets/images/coin-creatif.png"/>
+            <Carousel.Caption>
+              <h3>Coin Creatif</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
+          <Carousel.Item style={{'height': "300px"}}>
+            <img style={{'height': "300px"}} className="d-block w-100" src="/assets/images/google_book_search.jpg"/>
+            <Carousel.Caption>
+              <h3>Google Book Search</h3>
+            </Carousel.Caption>
+          </Carousel.Item>
           <Carousel.Item style={{'height': "300px"}}>
             <img style={{'height': "300px"}} className="d-block w-100" src="/assets/images/fooddadabase.png"/>
             <Carousel.Caption>
@@ -71,109 +83,6 @@ function bootstrapCarousel() {
         </Carousel>
 
       </div>
-
-      <Table striped bordered hover variant="dark">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Project Name</th>
-            <th>Github Repository</th>
-            <th>Deployed Link</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Food Dad-a-Base</td>
-            <td>
-              <a href="https://github.com/goantonioUW/food-dadabase">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://food-dad-a-base.herokuapp.com/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Weather Dashboard</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Weather-Dashboard">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/Weather-Dashboard/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Password Generator</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Password-Generator">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/Password-Generator/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>4</td>
-            <td>Eat Da Burger</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Burger">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://eat-or-not-burger.herokuapp.com/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>5</td>
-            <td>Kitchen Helper</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Kitchen-Helper">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/Kitchen-Helper/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>6</td>
-            <td>Day Planner</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Day-Planner">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/Day-Planner/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>7</td>
-            <td>Coding Quiz</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Code-Quiz">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/Code-Quiz/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>8</td>
-            <td>Employee Directory</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/employeedirectory">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://jkaganovsky.github.io/employeedirectory/">Deployed Site</a>
-            </td>
-          </tr>
-          <tr>
-            <td>9</td>
-            <td>Budget Tracker</td>
-            <td>
-              <a href="https://github.com/jkaganovsky/Budget-Tracker">Github Repository</a>
-            </td>
-            <td>
-              <a href="https://on-off-budget-tracking.herokuapp.com/">Deployed Site</a>
-            </td>
-          </tr>
-        </tbody>
-      </Table>
     </div>
   )
 };
