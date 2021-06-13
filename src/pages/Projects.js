d37ce809f676d506add15faf233828e16fb2f533
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-06-12 23:44:07 -0700

    Updated Projects page to card format and limited number of projects to less than 5.

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,15 +1,13 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import Nav from "../components/Nav";
-import ProjectsCarousel from "../components/ProjectsCarousel";
-import ProjectsTable from "../components/ProjectsTable";
+import ProjectsCard from "../components/ProjectsCard";
 
 /* eslint-disable jsx-a11y/alt-text */
 function Projects() {
   return(
     <>
       <Nav />
-      <ProjectsCarousel />
-      <ProjectsTable />
+      <ProjectsCard />
     </>
   )
 }
