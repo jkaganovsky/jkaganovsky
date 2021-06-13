eaa7a7db66789de8f89202dc1928254b6cd224e9
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-06-12 23:44:32 -0700

    Updated resume pdf file.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,20 +1,21 @@
 import { Document, Page } from "react-pdf"
 import Nav from "../components/Nav";
 import ResumePDF from "../documents/ResumePDF.pdf";
-import CardColumns from "react-bootstrap/CardColumns";
 
 function Resume() {
   return (
     <>
       <Nav />
       <div className="container-fluid d-flex justify-content-center">
+
         <Document
         file={ResumePDF}
         options={{ workerSrc: "/pdf.worker.js" }}
         >
+
           <Page pageNumber={1} />
           <Page pageNumber={2} />
-
+          
         </Document>
 
       </div>
