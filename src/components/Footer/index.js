6e76b80f0021b7033cdb330aab874739e8dd9fb1
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-06-12 23:43:13 -0700

    Added email address to footer.

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,7 +3,7 @@ function Footer() {
     <>
       <div className="card-footer bg-white text-center">
         <span className="iconify" data-icon="noto-v1:copyright" data-inline="false"></span>
-        2021 by Jailanie Kaganovsky
+        2021 by Jailanie Kaganovsky | jnctw33ty@gmail.com
       </div>
     </>
   )
