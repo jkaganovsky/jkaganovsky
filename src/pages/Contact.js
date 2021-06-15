4c862cac387ff35086d74baf8fa242f539907ae6
Jailanie Kaganovsky <jnctw33ty@gmail.com>
2021-06-14 19:45:06 -0700

    Added Knewave font to titles on each page, and reformatted for mobile friendly view of Contact page.

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,35 +1,31 @@
 import Nav from "../components/Nav";
+import { Card, Media } from "react-bootstrap";
 
 function Contact() {
   return(
     <>
     <Nav />
-      <div className="container-fluid d-flex flex-wrap justify-content-center p-5 m-5">
-        <div className="col-8 my-5 bg-white">
-
-            <p className="fs-1 text-center pt-2">CONTACT ME</p>
-
-            <a className="nav-link h5 text-center" href="mailto:jnctw33ty@gmail.com">
-              <span className="iconify" data-icon="entypo:email" data-inline="false"></span>
-              E-mail: jnctw33ty@gmail.com
-            </a>
-
-            <a className="nav-link h5 text-center" href="https://www.linkedin.com/in/jkaganovsky/">
-              <span className="iconify" data-icon="mdi-linkedin" data-inline="false"></span>
-              LinkedIn: https://www.linkedin.com/in/jkaganovsky/
-            </a>
-
-            <a className="nav-link h5 text-center" href="https://github.com/jkaganovsky">
-              <span className="iconify" data-icon="entypo-social:github" data-inline="false"></span>
-              Github: https://github.com/jkaganovsky
-            </a>
-
-            <a className="nav-link h5 text-center" href="resume">
-              <span className="iconify" data-icon="zmdi:collection-pdf" data-inline="false"></span>
-              Resume: https://docs.google.com/document/d/1U63ko9pZ3dNeMCcuE7i5g2YfHOdJp4xmGFkPXX4xC2E/edit?usp=sharing
-            </a>
-        </div>
-      </div>
+    <Card className="text-center bg-white my-5 py-5">
+    <h5 className="contact text-center bg-white">CONTACT ME</h5>
+      <ul>
+      <a className="nav-link h5 container-fluid" href="mailto:jnctw33ty@gmail.com">
+        <span className="iconify" data-icon="entypo:email" data-inline="false" data-width="40" data-height="40"></span>
+        E-mail: jnctw33ty@gmail.com
+      </a>
+      <a className="nav-link h5" href="https://www.linkedin.com/in/jkaganovsky/">
+        <span className="iconify" data-icon="mdi-linkedin" data-inline="false" data-width="40" data-height="40"></span>
+          LinkedIn: https://www.linkedin.com/in/jkaganovsky/
+      </a>
+      <a className="nav-link h5" href="https://github.com/jkaganovsky">
+          <span className="iconify" data-icon="entypo-social:github" data-inline="false" data-width="40" data-height="40"></span>
+          Github: https://github.com/jkaganovsky
+      </a>
+      <a className="nav-link h5" href="resume">
+        <span className="iconify" data-icon="zmdi:collection-pdf" data-inline="false" data-width="40" data-height="40"></span>
+        Resume: https://docs.google.com/document/d/1U63ko9pZ3dNeMCcuE7i5g2YfHOdJp4xmGFkPXX4xC2E/edit?usp=sharing
+      </a>
+      </ul>
+    </Card>
     </>
   )
 }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -5,18 +5,18 @@ function Main() {
       <div className="card my-5 py-5 bg-white">
         <div className="row">
           <div className="col-lg">
-            <h1 className="name text-center"> JAILANIE KAGANOVSKY </h1>
+            <h1 className="name text-center my-5"> JAILANIE KAGANOVSKY </h1>
           </div>
         </div>
 
         <div className="row">
           <div className="col-lg">
-            <h2 className="title text-center">Full-Stack Web Developer & Research Coordinator</h2>
+            <h2 className="title text-center">Full-Stack Web Developer | Research Coordinator</h2>
           </div>
         </div>
       </div>
 
-      <div className="container-fluid pb-5">
+      <div className="container-fluid pb-5 m-6">
         <div className="d-flex flex-wrap justify-content-evenly">
           <a className="text-white fw-bolder fs-3 p-4" href="about">ABOUT ME</a>
           <a className="text-white fw-bolder fs-3 p-4" href="projects">PROJECTS</a>
